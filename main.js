0fb9c4b3b08501fbd2b1e919e3687e4c6197065a
TrvMarco <trevisanello.marco@gmail.com>
2022-04-29 15:37:31 +0200

    aggiunto script per apertura e chiusura hamburger menu

---
+++ main.js
@@ -0,0 +1,25 @@
+// trasformare l'elemento open in una variabile 
+const button = document.getElementById("open-menu");
+
+// aggiungere azione al click dell'elemento
+let hamburgerMenu = document.getElementById("hamburger-menu")
+
+button.addEventListener('click',
+
+    function(){
+        hamburgerMenu.style.display = "block";
+    }
+
+);
+
+// per chiudere il menù trasformare elemento close in una variabile
+const closeButton =  document.getElementById("close-menu")
+
+// aggiungere azione al click dell'elemento
+closeButton.addEventListener('click',
+
+    function(){
+        hamburgerMenu.style.display = "none";
+    }
+
+);
